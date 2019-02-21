bf1d7cf1011ccc57104328008ac87e33b9b73f93
Pavel Neizhmak <paul.neizhmak@gmail.com>
2019-02-21 17:43:17 +0300

    proceed with channels logic

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -10,16 +10,21 @@
             $http.get('https://cors-anywhere.herokuapp.com/http://yasna.by')
                 .success(function (data) {
                     alert("success: see console");
-                    // console.log(data);
 
                     let domData = new DOMParser().parseFromString(data, "text/html");
-
                     let yasnaModals = domData.querySelectorAll('[id^=Modal]');
 
                     console.log(yasnaModals);
 
                     $scope.yasnaModalsFormatted = [];
                     angular.forEach(yasnaModals,function(value,index){
+
+                        if (value.querySelector('#myModalLabel').textContent === $rootScope.yasnaPlan.name) {
+                            alert('Selected plan is equals to yasna.by plan');
+                            console.log(value.querySelector('#myModalLabel').textContent + ' --equals to--- ' + $rootScope.yasnaPlan.name)
+
+                            //todo: value.querySelectorAll('[id^=Modal] > div > div > div.modal-body > div')
+                        }
                         $scope.yasnaModalsFormatted.push(value.id);
                     });
 
--- app/personal/personal.controller.js
+++ app/personal/personal.controller.js
@@ -12,15 +12,16 @@
                 availableOptions: [
                     {id: '1', name: 'Ясна Лайт'},
                     {id: '2', name: 'Ясна 35'},
-                    {id: '3', name: 'Ясна 35+'},
+                    {id: '3', name: 'Ясна 35 плюс'},
                     {id: '4', name: 'Ясна 50'},
-                    {id: '5', name: 'Ясна 50 smart'},
+                    {id: '5', name: 'Ясна 50 SMART'},
                     {id: '6', name: 'Ясна 75'},
-                    {id: '7', name: 'Ясна 75+'},
+                    {id: '7', name: 'Ясна 75 плюс'},
                     {id: '8', name: 'Ясна 100'},
-                    {id: '9', name: 'Ясна 100 smart'},
-                    {id: '10', name: 'Ясна игровой'},
-                    {id: '11', name: 'Ясна игровой smart'}
+                    {id: '9', name: 'Ясна 100 SMART'},
+                    {id: '10', name: 'Ясна 200'},
+                    {id: '11', name: 'Игровой'},
+                    {id: '12', name: 'Игровой Smart'}
                 ],
                 selectedOption: {id: '1', name: 'Ясна Лайт'}
             };
