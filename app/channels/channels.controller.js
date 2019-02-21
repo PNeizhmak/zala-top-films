d90c589f401132ccfcd1ef3178de0c31689f04b4
NeizhmakTI <TNeizhmak@qulix.com>
2019-02-22 00:06:19 +0300

    display the channels from yasna.by

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -2,34 +2,32 @@
     'use strict';
 
     angular.module('wizardApp')
-        .controller('ChannelsController', ['$rootScope','$scope', '$http', function ($rootScope, $scope, $http) {
+        .controller('ChannelsController', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
 
             let vm = this;
             vm.title = 'Here is the list of your TV channels';
 
             $http.get('https://cors-anywhere.herokuapp.com/http://yasna.by')
                 .success(function (data) {
-                    alert("success: see console");
+                    alert("Got the response from yasna.by");
 
                     let domData = new DOMParser().parseFromString(data, "text/html");
                     let yasnaModals = domData.querySelectorAll('[id^=Modal]');
 
-                    console.log(yasnaModals);
-
-                    $scope.yasnaModalsFormatted = [];
-                    angular.forEach(yasnaModals,function(value,index){
-
+                    $scope.channels = [];
+                    angular.forEach(yasnaModals, function (value, index) {
                         if (value.querySelector('#myModalLabel').textContent === $rootScope.yasnaPlan.name) {
                             alert('Selected plan is equals to yasna.by plan');
-                            console.log(value.querySelector('#myModalLabel').textContent + ' --equals to--- ' + $rootScope.yasnaPlan.name)
 
-                            //todo: value.querySelectorAll('[id^=Modal] > div > div > div.modal-body > div')
+                            let channelsDom = value.querySelectorAll('a');
+                            $scope.channelsSize = channelsDom.length;
+
+                            angular.forEach(channelsDom, function (value, index) {
+                                console.log(index + '-' + value.text);
+                                $scope.channels.push(value.text);
+                            });
                         }
-                        $scope.yasnaModalsFormatted.push(value.id);
                     });
-
-                    console.log($scope.yasnaModalsFormatted);
-
                 })
                 .error(function (data) {
                     alert(data);
