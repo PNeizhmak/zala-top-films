4a576b12287c93a79c5f07468a99387ff2e4d1e6
NeizhmakTI <TNeizhmak@qulix.com>
2019-02-27 00:15:18 +0300

    initial films retrievement #2

--- app/kinopoisk/kinopoisk.controller.js
+++ app/kinopoisk/kinopoisk.controller.js
@@ -12,28 +12,15 @@
                 let filmTitle = value.filmTitle;
 
                 let headers = new Headers();
-                headers.append('Content-Type', 'application/json');
-                headers.append('origin', 'x-requested-with');
-                headers.append('X-Requested-With', '');
-                headers.append('XMLHttpRequest', '');
-                headers.append('Host', 'www.kinopoisk.ru');
-                headers.append('Accept', '*/*');
-                headers.append('User-Agent', 'Mozilla/5.0');
+                headers.append('Access-Control-Allow-Origin', '*');
 
-                let params = new URLSearchParams();
-                params.append("q", filmTitle);
-                params.append("topsuggest", 'true');
-                params.append("ajax", '1');
-
-                $http.get('https://cors-anywhere.herokuapp.com/https://www.kinopoisk.ru/handler_search.php?q=Ирландец&topsuggest=true', {headers: headers})
+                $http.get('https://cors-anywhere.herokuapp.com/https://www.kinopoisk.ru/handler_search.php?q=' + filmTitle + '&topsuggest=true', {headers: headers})
                     .success(function (data) {
-
-                        let domData = new DOMParser().parseFromString(data, "text/html");
-                        let domChannels = domData.getElementsByClassName('channel');
-
+                        console.log('-----------');
+                        console.log(filmTitle);
+                        console.log(data);
                     })
                     .error(function (data) {
-                        alert(data);
                         console.log('Error: ' + data);
                     });
 
