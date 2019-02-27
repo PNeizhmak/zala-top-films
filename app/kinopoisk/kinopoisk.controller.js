7d9f1f8d8e453a2c715020440aaa4dc006b3be05
NeizhmakTI <TNeizhmak@qulix.com>
2019-02-27 22:34:17 +0300

    console films retrievement

--- app/kinopoisk/kinopoisk.controller.js
+++ app/kinopoisk/kinopoisk.controller.js
@@ -2,10 +2,12 @@
     'use strict';
 
     angular.module('wizardApp')
-        .controller('KinopoiskController', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+        .controller('KinopoiskController', ['$rootScope', '$scope', '$http', '$q', function ($rootScope, $scope, $http, $q) {
 
             let vm = this;
             vm.title = 'Here is the list of films sorted by kinopoisk raiting';
+
+            $scope.kinopoiskData = [];
 
             angular.forEach($rootScope.films, function (value, index) {
 
@@ -16,14 +18,24 @@
 
                 $http.get('https://cors-anywhere.herokuapp.com/https://www.kinopoisk.ru/handler_search.php?q=' + filmTitle + '&topsuggest=true', {headers: headers})
                     .success(function (data) {
-                        console.log('-----------');
-                        console.log(filmTitle);
-                        console.log(data);
+                        let filmData = {
+                            channelName: value.channelName,
+                            filmTime: value.filmTime,
+                            filmTitle: filmTitle,
+                            kinopoisk: data
+                        };
+                        $scope.kinopoiskData.push(filmData);
+                        console.log(filmData);
                     })
                     .error(function (data) {
                         console.log('Error: ' + data);
                     });
+                $scope.promise = $http.get('https://cors-anywhere.herokuapp.com/https://www.kinopoisk.ru/handler_search.php?q=' + filmTitle + '&topsuggest=true', {headers: headers});
+            });
 
+            $scope.promise.then(function () {
+                console.log($scope.kinopoiskData);
+                alert('Enjoy the result !')
             });
         }]);
 })();
